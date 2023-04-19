8887745588885a8efed7b96b34b40ab445ec1ca5
Khoa Duong <mr_jupipro@yahoo.com>
2023-04-20 02:17:53 +0700

    Create Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <section className="bg-[#414141] w-full">
+      <div className="max-w-screen-lg h-32 mx-auto px-8 text-base text-white font-light flex items-center">
+        <ul className="flex gap-12 text-[11px]">
+          <li>会員登録</li>
+          <li>運営会社</li>
+          <li>利用規約</li>
+          <li>個人情報の取扱について</li>
+          <li>特定商取引法に基づく表記</li>
+          <li>お問い合わせ</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
