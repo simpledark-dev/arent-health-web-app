8fface508cb247019be84b4a0b9c6e599ce760a9
Khoa Duong <mr_jupipro@yahoo.com>
2023-04-20 02:19:55 +0700

    Fix styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,20 +4,25 @@ import iconMemo from "@/public/assets/icon_memo.svg";
 import iconChallenge from "@/public/assets/icon_challenge.svg";
 import iconInfo from "@/public/assets/icon_info.svg";
 import iconMenu from "@/public/assets/icon_menu.svg";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <section className="bg-[#414141] w-full">
-      <div className="max-w-screen-lg mx-auto text-base text-white font-light">
+      <div className="max-w-screen-lg mx-auto px-8 text-base text-white font-light">
         <nav className="flex justify-between h-16 items-center">
           <div className="mt-2">
-            <Image height={40} src={healthLogo} alt="logo" />
+            <Link href="/top-page">
+              <Image height={40} src={healthLogo} alt="logo" />
+            </Link>
           </div>
           <ul className="flex gap-12">
-            <li className="flex gap-1 items-center">
-              <Image height={32} src={iconMemo} alt="icon-memo" />
-              <span>自分の記録</span>
-            </li>
+            <Link href="/record-page">
+              <li className="flex gap-1 items-center">
+                <Image height={32} src={iconMemo} alt="icon-memo" />
+                <span>自分の記録</span>
+              </li>
+            </Link>
             <li className="flex gap-1 items-center">
               <Image height={32} src={iconChallenge} alt="icon-challenge" />
               <span>チャレンジ</span>
--- components/TopPage/MealHistory.tsx
+++ components/TopPage/MealHistory.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Image from "next/image";
 import ClipLoader from "react-spinners/ClipLoader";
 import morningHex from "@/public/assets/morning_component_hex.svg";
@@ -9,31 +10,30 @@ import useFetchMealHistory from "@/hooks/useFetchMealHistory";
 
 import { formateDateMonth } from "@/utils/dateTime";
 import { Meal } from "@/types";
-import { useState } from "react";
 
 const FilterButtons = ({ setFilter }: { setFilter: any }) => {
   return (
     <div className="flex justify-center gap-16">
       <Image
-        className="hover:translate-x-1 hover:-translate-y-1 hover:shadow-lg"
+        className="hover:translate-x-1 hover:-translate-y-1"
         src={morningHex}
         alt="morning-hex"
         onClick={() => setFilter("Morning")}
       />
       <Image
-        className="hover:translate-x-1 hover:-translate-y-1 hover:shadow-lg"
+        className="hover:translate-x-1 hover:-translate-y-1"
         src={lunchHex}
         alt="lunch-hex"
         onClick={() => setFilter("Lunch")}
       />
       <Image
-        className="hover:translate-x-1 hover:-translate-y-1 hover:shadow-lg"
+        className="hover:translate-x-1 hover:-translate-y-1"
         src={dinnerHex}
         alt="dinner-hex"
         onClick={() => setFilter("Dinner")}
       />
       <Image
-        className="hover:translate-x-1 hover:-translate-y-1 hover:shadow-lg"
+        className="hover:translate-x-1 hover:-translate-y-1"
         src={snackHex}
         alt="snack-hex"
         onClick={() => setFilter("Snack")}
@@ -44,7 +44,7 @@ const FilterButtons = ({ setFilter }: { setFilter: any }) => {
 
 const MealList = ({ mealList }: { mealList: Meal[] | null }) => {
   return (
-    <>
+    <section>
       <div className="flex flex-wrap justify-center gap-2">
         {mealList &&
           mealList.map((meal) => {
@@ -53,13 +53,13 @@ const MealList = ({ mealList }: { mealList: Meal[] | null }) => {
             return (
               <div key={meal.id} className="relative ">
                 <Image
-                  className=" w-56 aspect-square object-cover"
+                  className="w-[14.625rem] aspect-square object-cover"
                   src={meal.image}
                   alt="meal"
                 />
                 <div
                   className="absolute bottom-0 left-0 w-1/2 h-8 bg-[#FFCC21] flex justify-center items-center
-            text-white font-light text-[0.9rem]"
+                text-white font-light text-[0.9rem]"
                 >
                   {formateDateMonth(meal.time.date, meal.time.month)}.
                   {meal.type}
@@ -76,12 +76,12 @@ const MealList = ({ mealList }: { mealList: Meal[] | null }) => {
             background:
               "linear-gradient(32.95deg, #FFCC21 8.75%, #FF963C 86.64%)",
           }}
-          className="block h-14 w-72 bg-black text-white font-light rounded mt-6 mx-auto"
+          className="block h-14 w-72 bg-black text-white font-light rounded my-12 mx-auto"
         >
           記録をもっと見る
         </button>
       )}
-    </>
+    </section>
   );
 };
 
@@ -94,7 +94,7 @@ const MealHistory = () => {
     : [];
 
   return (
-    <section className="max-w-screen-lg mx-auto my-8 space-y-6">
+    <section className="max-w-screen-lg mx-auto mt-8 mb-16 space-y-6">
       <FilterButtons setFilter={setFilter} />
       {loading ? (
         <div className="w-max mx-auto py-32">
